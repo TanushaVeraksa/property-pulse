54ed7d780c61cebfa6ba54a8b35d33c11cf86a8b
TanushaVeraksa <veraksa161@gmail.com>
2025-08-08 12:51:33 +0300

    feat: fetch single property

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,7 +1,7 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 
-// GET /api//properties
+// GET /api/properties
 export const GET = async (request) => {
   try {
     await connectDB();
