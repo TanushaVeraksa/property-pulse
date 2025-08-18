64da6f0c6bd23a1c47c7175d3735151a5993fa0a
TanushaVeraksa <veraksa161@gmail.com>
2025-08-18 16:28:02 +0300

    feat: add toast notification

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,8 @@ import Navbar from "@/components/Navbar";
 import "@/assets/styles/globals.css";
 import Footer from "../components/Footer";
 import AuthProvider from "@/components/AuthProvider";
+import React from "react";
+import { ToastContainer } from "react-toastify";
 
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
@@ -17,6 +19,7 @@ const MainLayout = ({ children }) => {
           <Navbar />
           <main>{children}</main>
           <Footer />
+          <ToastContainer />
         </body>
       </html>
     </AuthProvider>
