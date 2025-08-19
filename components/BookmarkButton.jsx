789f3f87499a706c79767676be8114379cbfff71
TanushaVeraksa <veraksa161@gmail.com>
2025-08-19 17:02:40 +0300

    feat: check bookmark status & button toggle

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -7,8 +7,39 @@ import { FaBookmark } from "react-icons/fa";
 const BookmarkButton = ({ property }) => {
   const { data: session } = useSession();
   const userId = session?.user?.id;
-  const [isBokmarked, setIsBookmarked] = useState(false);
-  console.log(property);
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!userId) {
+      setLoading(false);
+      return;
+    }
+    const checkBookmarkStatus = async () => {
+      try {
+        const res = await fetch("/api/bookmarks/check", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            propertyId: property._id,
+          }),
+        });
+        console.log("RES", res);
+        if (res.status === 200) {
+          const data = await res.json();
+
+          setIsBookmarked(data.isBookmarked);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkBookmarkStatus();
+  }, [property._id, userId]);
 
   const handleClick = async () => {
     if (!userId) {
@@ -29,14 +60,24 @@ const BookmarkButton = ({ property }) => {
       if (res.status === 200) {
         const data = await res.json();
         toast.success(data.message);
-        setIsBookmarked(data.setIsBookmarked);
+        setIsBookmarked(data.isBookmarked);
       }
     } catch (error) {
       console.log(error);
       toast.error("Something went wrong");
     }
   };
-  return (
+
+  if (loading) return <p className="text-center">Loading...</p>;
+
+  return isBookmarked ? (
+    <button
+      onClick={handleClick}
+      className="cursor-pointer bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className="mr-2" /> Remove Bookmark
+    </button>
+  ) : (
     <button
       onClick={handleClick}
       className="cursor-pointer bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
