76b87bc056e9d58e880b301e8a870ce471d09f19
TanushaVeraksa <veraksa161@gmail.com>
2025-08-19 15:16:32 +0300

    style: add cursor pointer in to buttons

--- components/PropertyContactForm.jsx
+++ components/PropertyContactForm.jsx
@@ -68,7 +68,7 @@ const PropertyContactForm = () => {
         </div>
         <div>
           <button
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center"
+            className="cursor-pointer bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center"
             type="submit"
           >
             <FaPaperPlane className="mr-2" /> Send Message
